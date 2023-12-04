35568156c89e31d36a97f4f71f9333188c7947a1
yeondori <yeonseo.jung0@gmail.com>
2023-12-04 16:09:59 +0900

    feat: 생성자 추가

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -3,4 +3,9 @@ package lotto;
 public class WinningLotto {
     private final Lotto lotto;
     private final int bonus;
+
+    public WinningLotto(Lotto lotto, int bonus) {
+        this.lotto = lotto;
+        this.bonus = bonus;
+    }
 }
