5be16a6eef5d2ede5fb4c953e05ee98363e2ce3c
yeondori <yeonseo.jung0@gmail.com>
2023-12-04 16:21:34 +0900

    feat: 검증 로직 추가

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -5,7 +5,25 @@ public class WinningLotto {
     private final int bonus;
 
     public WinningLotto(Lotto lotto, int bonus) {
+        validate(lotto, bonus);
         this.lotto = lotto;
         this.bonus = bonus;
+    }
+
+    private void validate(Lotto lotto, int bonus) {
+        validateRange(bonus);
+        validateDuplicateWithLotto(lotto, bonus);
+    }
+
+    private void validateRange(int number) {
+        if (number<1 || number>45) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicateWithLotto(Lotto lotto, int bonus) {
+        if (lotto.hasNumber(bonus)) {
+            throw new IllegalArgumentException();
+        }
     }
 }
