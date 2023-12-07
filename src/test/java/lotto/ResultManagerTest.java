7b1672545631ab5b4603bbfa93e2b8852ab1218f
yeondori <yeonseo.jung0@gmail.com>
2023-12-07 17:09:42 +0900

    feat: getResult 테스트(2등인 경우)

--- src/test/java/lotto/ResultManagerTest.java
+++ src/test/java/lotto/ResultManagerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ResultManagerTest {
 
@@ -23,5 +22,22 @@ class ResultManagerTest {
         Result lottoResult = resultManager.getLottoResult();
         //then
         assertThat(lottoResult).isEqualTo(Result.FIRST_PRIZE);
+    }
+
+    @DisplayName("보너스 번호를 포함해 6개 번호가 일치하는 경우 2등을 반환한다.")
+    @Test
+    void getResultWithSecondPrize() {
+        //given
+        Lotto lotto = Lotto.from(Arrays.asList(1, 2, 3, 4, 5, 7));
+        Ball bonusBall = new Ball(7);
+        Lotto winningNumbers = Lotto.from(Arrays.asList(1, 2, 3, 4, 5, 8));
+
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusBall);
+        //when
+        ResultManager resultManager = new ResultManager(lotto, winningLotto);
+
+        Result lottoResult = resultManager.getLottoResult();
+        //then
+        assertThat(lottoResult).isEqualTo(Result.SECOND_PRIZE);
     }
 }
