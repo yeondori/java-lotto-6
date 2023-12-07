cb02110f1f70d193ce55369916756b197014c7fb
yeondori <yeonseo.jung0@gmail.com>
2023-12-07 16:23:52 +0900

    feat: getResult 테스트(낙첨인 경우)

--- src/test/java/lotto/ResultTest.java
+++ src/test/java/lotto/ResultTest.java
@@ -3,7 +3,11 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -37,11 +41,37 @@ class ResultTest {
         assertThat(result).isEqualTo(Result.FIFTH_PRIZE);
     }
 
+    @DisplayName("보너스 숫자여부와 관계없이 일치하는 개수가 3 미만일 경우 낙첨이다.")
+    @ParameterizedTest
+    @MethodSource("provideParameters")
+    void getResultWithNonePrize(int matchNumbers, boolean matchBonus) throws Exception {
+        Result result = Result.getResult(matchNumbers, matchBonus);
+        assertThat(result).isEqualTo(Result.NONE);
+    }
+
+    private static Stream<Arguments> provideParameters() {
+        return Stream.of(
+                Arguments.of(2, true),
+                Arguments.of(1, true),
+                Arguments.of(0, true),
+                Arguments.of(2, false),
+                Arguments.of(1, false),
+                Arguments.of(0, false)
+        );
+    }
+
     @DisplayName("보너스 숫자가 일치하고, 일치하는 개수가 5일 경우 2등이다.")
     @Test
     void getResultWithSecondPrize() throws Exception {
         Result result = Result.getResult(SECOND_AND_THIRD_PRIZE_MATCHING_NUM, true);
         assertThat(result).isEqualTo(Result.SECOND_PRIZE);
+    }
+
+    @DisplayName("보너스 숫자가 일치하지 않고, 일치하는 개수가 5일 경우 3등이다.")
+    @Test
+    void getResultWithThirdPrize() throws Exception {
+        Result result = Result.getResult(SECOND_AND_THIRD_PRIZE_MATCHING_NUM, false);
+        assertThat(result).isEqualTo(Result.THIRD_PRIZE);
     }
 
 
