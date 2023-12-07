6e1f1620a1d046e74c426da78c9f4a25f1aa0866
yeondori <yeonseo.jung0@gmail.com>
2023-12-07 17:07:17 +0900

    feat: getResult 테스트

---
+++ src/test/java/lotto/ResultManagerTest.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResultManagerTest {
+
+    @DisplayName("모든 번호가 일치하는 경우 1등을 반환한다.")
+    @Test
+    void getResult() {
+        //given
+        Lotto lotto = Lotto.from(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Ball bonusBall = new Ball(7);
+        WinningLotto winningLotto = new WinningLotto(lotto, bonusBall);
+        //when
+        ResultManager resultManager = new ResultManager(lotto, winningLotto);
+
+        Result lottoResult = resultManager.getLottoResult();
+        //then
+        assertThat(lottoResult).isEqualTo(Result.FIRST_PRIZE);
+    }
+}
