5c0c4dcfcd7e0ad46531e5a57ad08509ef9df5fa
yeondori <yeonseo.jung0@gmail.com>
2023-12-07 17:44:51 +0900

    refactor: LOTTO_PRICE 상수화

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -3,22 +3,24 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoGenerator {
+
+    private static final int LOTTO_PRICE = 1_000;
+
     public List<Lotto> getLottos(int amount) {
         validateAmount(amount);
-        return IntStream.range(0, amount)
+        return IntStream.range(0, amount/LOTTO_PRICE)
                 .mapToObj(i -> Lotto.from(getRandomNumbers()))
-                .collect(Collectors.toList());
+                .toList();
     }
 
     private void validateAmount(int amount) {
-        if (amount < 1000) {
+        if (amount < LOTTO_PRICE) {
             throw new IllegalArgumentException("로또 최소 구매금액은 1,000원입니다");
         }
-        if ((amount % 1000) != 0) {
+        if ((amount % LOTTO_PRICE) != 0) {
             throw new IllegalArgumentException("로또는 1,000원 단위로 구매 가능합니다.");
         }
     }
