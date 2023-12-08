213106624073d72d630e97374c19eb15aa58305e
yeondori <yeonseo.jung0@gmail.com>
2023-12-08 20:54:20 +0900

    feat: getAllResult 기능 추가

--- src/main/java/lotto/ResultManager.java
+++ src/main/java/lotto/ResultManager.java
@@ -1,31 +1,42 @@
 package lotto;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class ResultManager {
-    private final Lotto lotto;
     private final WinningLotto winningLotto;
 
-    public ResultManager(Lotto lotto, WinningLotto winningLotto) {
-        this.lotto = lotto;
+    public ResultManager(WinningLotto winningLotto) {
         this.winningLotto = winningLotto;
     }
 
-    public Result getLottoResult() {
+    public Map<Result, Integer> getAllResults(List<Lotto> lottos) {
+        return lottos.stream()
+                .collect(Collectors.toMap(
+                        this::getLottoResult,
+                        result -> 1,
+                        Integer::sum
+                ));
+    }
+
+    public Result getLottoResult(Lotto lotto) {
         Lotto winningNumbers = winningLotto.getLotto();
         Ball bonusBall = winningLotto.getBonus();
 
-        int matchNumber = countMatchingNumbers(winningNumbers);
-        boolean matchBonus = hasMatchBonus(bonusBall);
+        int matchNumber = countMatchingNumbers(lotto, winningNumbers);
+        boolean matchBonus = hasMatchBonus(lotto, bonusBall);
 
         return Result.getResult(matchNumber, matchBonus);
     }
-    private int countMatchingNumbers(Lotto winningNumbers) {
+    private int countMatchingNumbers(Lotto lotto, Lotto winningNumbers) {
         return lotto.getNumbers().stream()
                 .filter(winningNumbers::hasBall)
                 .toList()
                 .size();
     }
 
-    public boolean hasMatchBonus(Ball bonusBall) {
+    private boolean hasMatchBonus(Lotto lotto, Ball bonusBall) {
         return lotto.hasBall(bonusBall);
     }
 }
